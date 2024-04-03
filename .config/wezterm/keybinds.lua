d5ec44f3bce3d10d0053819cb6f7fb500a3fe0f8
Klingspor, Christoph <klingspor@gmx.net>
2024-04-03 09:40:20 +0200

    Allow word-forward and word-backward according to https://github.com/wez/wezterm/issues/253

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -143,6 +143,18 @@ function module.apply_to_config(config)
     mods = "CTRL|SHIFT",
     action = action{SplitVertical = {domain="CurrentPaneDomain"}},
   },
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {
+    key="LeftArrow",
+    mods="OPT",
+    action=wezterm.action{SendString="\x1bb"}
+  },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {
+    key="RightArrow", 
+    mods="OPT", 
+    action=wezterm.action{SendString="\x1bf"}
+  },  
 }
   -- Disable dead keys
   config.use_dead_keys = false
