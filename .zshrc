0b11129f1a7e1150af5a397d0096009dc86dfd77
Klingspor, Christoph <klingspor@gmx.net>
2024-04-16 17:58:01 +0200

    Alias cat to bat

---
+++ .zshrc
@@ -0,0 +1,4 @@
+alias cat='bat'
+
+PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
+export PATH=$HOME/bin:/opt/homebrew/bin:/usr/local/bin:$PATH
--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Install bat & alias to cat
+brew install bat
+
 # TODO: Handle if .config alread not already exist?
 ln -s $PWD/.config/wezterm $HOME/.config/wezterm
 ln -s $PWD/.vimrc $HOME/.vimrc
+
+
+# TODO Create custom .zshrc that contains alias cat='bat' 
+ln -s /.zshrc  ~/.zshrc
