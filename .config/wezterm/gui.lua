d00df0544c955a4fe55beab190df6906c20fe392
Klingspor, Christoph <klingspor@gmx.net>
2024-04-25 15:25:29 +0200

    Change opacity in gui.lua, add vscode config files to setup

--- .config/wezterm/gui.lua
+++ .config/wezterm/gui.lua
@@ -21,7 +21,7 @@ function module.apply_to_config(config)
   }
   config.use_fancy_tab_bar = false
   config.hide_tab_bar_if_only_one_tab = true
-  config.window_background_opacity = 0.95
+  config.window_background_opacity = 0.9
   config.scrollback_lines = 10000 
 end
 
--- install.sh
+++ install.sh
@@ -2,10 +2,22 @@
 # Install bat & alias to cat
 brew install bat
 
+## Symlinks
 # TODO: Handle if .config alread not already exist?
 ln -s $PWD/.config/wezterm $HOME/.config/wezterm
 ln -s $PWD/.vimrc $HOME/.vimrc
 
+# vscode
+# delete existing files
+if [ -f "$HOME/Library/Application\ Support/Code/User/keybindings.json" ]; then
+    rm "$HOME/Library/Application\ Support/Code/User/keybindings.json"
+fi
+ln -s $PWD/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+
+if [ -f "$HOME/Library/Application\ Support/Code/User/settings.json" ]; then
+    rm "$HOME/Library/Application\ Support/Code/User/settings.json"
+fi
+ln -s $PWD/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 # TODO Create custom .zshrc that contains alias cat='bat' 
 ln -s $PWD/.zshrc  $HOME/.zshrc
